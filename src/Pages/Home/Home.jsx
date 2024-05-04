d374825f07b701348f26418ff3652804ad7a9d06
soyebsuvo <soyebbeen@gmail.com>
2024-05-05 04:41:00 +0600

    project initialized

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../Components/Navbar/Navbar";
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
+};
+
+export default Home;
