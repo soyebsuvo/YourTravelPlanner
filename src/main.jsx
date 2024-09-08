0c35cb7754700a93985494a19d6e3bfe9a3a91c9
MohammedHamim <99599371+BlackdreamIO@users.noreply.github.com>
2024-09-08 14:02:36 +0600

    feat: marketplace ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './Routes/Router.jsx'
-import Context from './Components/Context/Context.jsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ChakraBaseProvider } from '@chakra-ui/react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { router } from './Routes/Router.jsx';
+
+import Context from './Components/Context/Context.jsx';
+
+import './index.css';
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ChakraBaseProvider disableGlobalStyle >
-    <QueryClientProvider client={queryClient}>
-        <Context>
-          <RouterProvider router={router} />
-        </Context>
-    </QueryClientProvider>
-    </ChakraBaseProvider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <Context>
+                <RouterProvider router={router} />
+            </Context>
+        </QueryClientProvider>
+    </React.StrictMode>,
 )
