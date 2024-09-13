8d519c89cf8f06c00df3c37fe2bafa43359adbf9
MohammedHamim <99599371+BlackdreamIO@users.noreply.github.com>
2024-09-13 12:52:54 +0600

    feat: shade-cn fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src', // Correctly points '@' to '/src'
+      'components': '/src/shadecn', // Correctly points 'components' to '/src/shadecn'
+    },
+  },
 })
